25aa9003b5bd2217f6d4fc967e2a41d7a59e6b5d
farcry6 <samsung.snow@gmail.com>
2021-02-25 18:48:28 +0100

    aaa

---
+++ condiciones_policia.py
@@ -0,0 +1,8 @@
+
+edad = int(input("escribe tu edad: "))
+altura = int(input("escribe tu altura en cm: 18"))
+if (edad >= 18 and altura >= 170):
+    print("cumples las condiciones")
+else:
+    print("lo sentimos no cumples con las condiciones entrena al baloncesto  y vuelva a intentar")
+
--- ejercicio_sumar.py
+++ ejercicio_sumar.py
@@ -4,4 +4,3 @@ numero2=float(input("Introduce es secondo numero "))
 suma=numero1+numero2
 
 print("La suma es: ",suma)
-1
